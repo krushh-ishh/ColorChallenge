84988b6e1f16b4bc7a8d3c57ffb09353c88ead26
kashish <kashish10700@gmail.com>
2024-02-20 00:26:32 +0530

    Color shuffle logic implemented

--- app/src/main/java/com/fun/codechallenge_app/ColorActivity.java
+++ app/src/main/java/com/fun/codechallenge_app/ColorActivity.java
@@ -2,13 +2,21 @@ package com.fun.codechallenge_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class ColorActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+public class ColorActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button button1, button2, button3, button4;
     private ImageButton pausePlayBtn;
@@ -20,9 +28,24 @@ public class ColorActivity extends AppCompatActivity {
         setContentView(R.layout.activity_color);
 
         initView();
+        gamePlay();
 
-        button1.setOnClickListener( v ->
-                Toast.makeText(this,"button1", Toast.LENGTH_LONG).show());
+    }
+
+    private void gamePlay() {
+        button1.setOnClickListener(this);
+        button2.setOnClickListener(this);
+        button3.setOnClickListener(this);
+        button4.setOnClickListener(this);
+    }
+    @Override
+    public void onClick(View v) {
+        List<String> colors = Arrays.asList("#FBFE7A", "#FF8D8D", "#7BCFCF", "#999DE7");
+        Collections.shuffle(colors);
+        button1.setBackgroundColor(Color.parseColor(colors.get(0)));
+        button2.setBackgroundColor(Color.parseColor(colors.get(1)));
+        button3.setBackgroundColor(Color.parseColor(colors.get(2)));
+        button4.setBackgroundColor(Color.parseColor(colors.get(3)));
     }
 
     private void initView() {
